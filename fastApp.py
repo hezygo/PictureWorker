0bc8c3e6b9e15053c575b28ecb8dd23ae2cc97cd
cm-lip <1573249948@qq.com>
2022-02-16 09:39:10 +0800

    :zap:新增是否下载图片的功能

--- fastApp.py
+++ fastApp.py
@@ -9,6 +9,7 @@
 
 
 from doctest import UnexpectedException
+from hashlib import md5
 from fastapi import FastAPI, APIRouter
 from pydantic import BaseModel, Field
 
@@ -34,20 +35,29 @@ app = APIRouter(
 
 @app.get('/getPic',
          response_model=PicResponse,
-         description='获取WeChat图片',
+         description='获取WeChat图片或者链接',
          name='获取WeChat图片'
          )
 async def get_general_train_task(
-        WcPageUrl: str = Query('', description='微信公众号文章链接')
+        WcPageUrl: str = Query('', description='微信公众号文章链接'),
+        IsWcPicDown: bool = Query(False, description='是否下载图片')
 ):
     ir = InitRequest()
     if WcPageUrl:
         text, _ = ir._respone(WcPageUrl)
     else:
         text = None
-    result = ir._process_down(ir.get_pic(text))
+    result = dict()
+    if IsWcPicDown:
+        result = ir._process_down(ir.get_pic(text))
+    else:
+        gp = ir.get_pic(text)
+        result = { util_md5(item): item for item in gp}
     return PicResponse(msg="成功", data=result)
 
+def util_md5(text):
+    pre =  md5(bytes(text,'utf-8'))
+    return pre.hexdigest()
 
 def set_app(application: FastAPI):
     InitLogger()
