8f16cb975d9743b9f5c2a84846369dc4e95ef9d7
cm-lip <1573249948@qq.com>
2022-02-16 12:05:27 +0800

    :pencil:新增md文档

--- fastApp.py
+++ fastApp.py
@@ -11,6 +11,7 @@
 from doctest import UnexpectedException
 from hashlib import md5
 from fastapi import FastAPI, APIRouter
+from py import code
 from pydantic import BaseModel, Field
 
 from getWeChatPic import InitRequest, InitLogger
@@ -26,6 +27,12 @@ class BaseResponse(BaseModel):
 class PicResponse(BaseResponse):
     data: dict = Field({}, description='路径')
 
+class MdBaseResponse(BaseModel):
+    title: str = Field(..., description='标题')
+    path: str = Field(..., description='路径')
+
+class MdResponse(BaseResponse):
+    data: MdBaseResponse = Field({}, description='文档数据')
 
 app = APIRouter(
     prefix="/WeChatPic",
@@ -38,7 +45,7 @@ app = APIRouter(
          description='获取WeChat图片或者链接',
          name='获取WeChat图片'
          )
-async def get_general_train_task(
+async def get_picture(
         WcPageUrl: str = Query('', description='微信公众号文章链接'),
         IsWcPicDown: bool = Query(False, description='是否下载图片')
 ):
@@ -52,12 +59,41 @@ async def get_general_train_task(
         result = ir._process_down(ir.get_pic(text))
     else:
         gp = ir.get_pic(text)
-        result = { util_md5(item): item for item in gp}
+        result = {util_md5(item): item for item in gp}
     return PicResponse(msg="成功", data=result)
 
+
+@app.get('/updateDayPicMd',
+         response_model=MdResponse,
+         description='更新图片MD',
+         name='更新图片MD'
+         )
+async def updateMyBlogsMd(
+        WcPageUrl: str = Query(..., description='微信公众号文章链接'),
+        MdName: str = Query(..., description='md文档名称')
+):
+    ir = InitRequest()
+    if WcPageUrl:
+        text, _ = ir._respone(WcPageUrl)
+    else:
+        return MdResponse(msg="更新失败", code=10422)
+    MD_IMG ='''![{image_name}]({url})\n'''
+    gp = ir.get_pic(text)
+    result = [ MD_IMG.format(image_name=util_md5(item), url=item) for item in gp]
+    util_write_md(MdName,result,ir.get_title(text))
+    return MdResponse(msg="成功",data={"title": ir.pages_title, "path": MdName })
+
+
 def util_md5(text):
-    pre =  md5(bytes(text,'utf-8'))
+    pre = md5(bytes(text, 'utf-8'))
     return pre.hexdigest()
+
+
+def util_write_md(name: str, text: list, head: str):
+    with open(f'{name}.md', 'w') as wf:
+        wf.write(head)
+        wf.writelines(text)
+
 
 def set_app(application: FastAPI):
     InitLogger()
@@ -65,3 +101,5 @@ def set_app(application: FastAPI):
     api.include_router(app)
     application.include_router(api)
     exception_init(application)
+
+# mv ./TestFirstFastAPI.md ../vuepress-starter/docs/TestFirstFastAPI.md
--- getWeChatPic.py
+++ getWeChatPic.py
@@ -16,6 +16,7 @@ import traceback
 import requests
 from bs4 import BeautifulSoup
 import os
+import re
 from loguru import logger
 from random import random
 from concurrent.futures import ThreadPoolExecutor, as_completed
@@ -23,7 +24,13 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 TMP = "./tmp"
 LOGDIR = 'log'
 PICDIR = 'pic'
-PIC_EXTENSION = {'image/jpg':'jpg', 'image/jpeg':'jpeg', 'image/gif':'gif', 'image/png':'png'}
+PIC_EXTENSION = {'image/jpg': 'jpg', 'image/jpeg': 'jpeg',
+                 'image/gif': 'gif', 'image/png': 'png'}
+MD_HEAD = '''---
+title: {title}
+author: {author}
+date: '{date}'
+---\n'''
 
 
 def InitLogger():
@@ -42,6 +49,7 @@ class InitRequest:
     def __init__(self) -> None:
         self.logger = logger
         self.find_all = []
+        self.pages_title = ''
 
     def _respone(self, url, beText=True, slp=1):
         sleep(slp)
@@ -50,16 +58,46 @@ class InitRequest:
             return resp.text, url
         return resp, url
 
-    def get_pic(self, text: str = None):
-
+    @staticmethod
+    def _check_build(text: str):
         if text:
-            soup = BeautifulSoup(text, 'lxml')
+            return BeautifulSoup(text, 'lxml')
         else:
-            self.logger.warning(traceback.format_exc())
             raise UnexpectedException(
                 "数据不存在", ValueError, traceback.format_exc())
-        for _,typ in PIC_EXTENSION.items():
-            
+
+    @staticmethod
+    def strip_all(text: str):
+        return text.lstrip().rstrip().replace("\n", "")
+
+    @property
+    def date_today(self):
+        return date.isoformat(datetime.now())
+
+    def get_title(self, text: str = None):
+        soup = self._check_build(text)
+        # title
+        p_title = soup.find(
+            attrs={"class": "rich_media_title", "id": "activity-name"})
+        self.pages_title = self.strip_all(
+            p_title.text) if p_title else f"{self.date_today}"
+        # date
+        pattern = re.compile(r'document.getElementById\("publish_time"\)')
+        script = soup.find("script", text=pattern)
+        p_dates = []
+        if script:
+            time_pattern = "\d{4}-\d{1,2}-\d{1,2}"
+            p_dates = re.findall(time_pattern, script.string)
+        p_date = p_dates[0] if p_dates else f"{self.date_today}"
+        # author
+        p_author = soup.find(attrs={"id": "js_name"})
+        p_author = self.strip_all(p_author.text)if p_author else "littlepig"
+        return MD_HEAD.format(title=self.pages_title, date=p_date, author=p_author)
+
+    def get_pic(self, text: str = None):
+        soup = self._check_build(text)
+        for _, typ in PIC_EXTENSION.items():
+
             res = soup.find_all(attrs={
                 "data-type": typ
             })
@@ -96,12 +134,15 @@ class InitRequest:
                     except Exception as err:
                         self.logger.warning(err)
                     else:
-                        ct_type =PIC_EXTENSION.get(result.headers['Content-Type'],None) 
-                        if ct_type :
-                            file_pname = self._save(result.content, f'.{ct_type}')
+                        ct_type = PIC_EXTENSION.get(
+                            result.headers['Content-Type'], None)
+                        if ct_type:
+                            file_pname = self._save(
+                                result.content, f'.{ct_type}')
                             rep[file_pname] = url
                         else:
-                            self.logger.warning(f"Unkown Content-Type:{result.headers['Content-Type']}")
+                            self.logger.warning(
+                                f"Unkown Content-Type:{result.headers['Content-Type']}")
                         del futures[future]
         return rep
 
@@ -113,8 +154,10 @@ class InitRequest:
             os.mkdir(f_dir)
         return today
 
+
 if __name__ == "__main__":
     ir = InitRequest()
-    # text, _ = ir._respone('https://mp.weixin.qq.com/s/B7o4OmJNZRuXsScX7wCl-A')
+    text, _ = ir._respone('https://mp.weixin.qq.com/s/B7o4OmJNZRuXsScX7wCl-A')
+    print(ir.get_title(text))
     # result = ir._process_down(ir.get_pic(text))
     # ir._process_down(ir.get_pic())
