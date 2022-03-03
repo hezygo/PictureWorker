2836793c7ccf360b1f5ffbfc218a12ff93b70bd4
cm-lip <1573249948@qq.com>
2022-03-03 11:01:58 +0800

    :zap:不自动找包

--- zero_rpc/setup.py
+++ zero_rpc/setup.py
@@ -1,9 +1,9 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
     name='zrpc',
     version='0.0.1',
-    packages=find_packages(),
+    packages=['zrpc'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
